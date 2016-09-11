caa4c4f5ae90d0ee87ad79c1d6daeb7bd9051365
adamandersen <adam.andersen@techbits.dk>
2016-09-11 21:20:22 +0200

    message

--- ex29.py
+++ ex29.py
@@ -1,27 +1,27 @@
-people = 20;
-cats = 30;
-dogs = 15;
+# set variables
+people = 24;
+cars = 25;
+trucks = 26;
 
-if people < cats:
-    print "To many cats! The world is doomed!"
+# if cars is more than pepole do
+if cars > people:
+    print "We should take the cars.";
+# if cars is less then people do
+elif cars < people:
+    print "We should not take the cars.";
 
-if people > cats:
-    print "Not many cats! The world is saved!"
+#if trucks has value grass then cars do
+if trucks > cars:
+    print "That's too many trucks.";
+# if trucks is less then do
+elif trucks < cars:
+    print "Maybe we could take the trucks.";
+# if no conditions meet do
+else:
+    print "We still can't decide.";
 
-if people < dogs:
-    print "The world is drooled on!"
-
-if people > dogs:
-    print "The world is dry!"
-
-dogs += 5
-
-if people >= dogs:
-    print "People are greater than or equal to dogs."
-
-if people <= dogs:
-    print "People are less than or equal to dogs."
-
-if people == dogs:
-    print "People are dogs."
-print 7*6
+# if people is greater then do
+if people > trucks:
+    print "Alright, let's just take the trucks.";
+else:
+    print "Fine, lets's stay home then."
---
+++ ex32.py
@@ -0,0 +1,8 @@
+# set list
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print "This is count %d" % number
